06cc76da01580faaabe6eba41ae27ff233fedef1
Stavratiy Andriy <stavraty@gmail.com>
2023-09-11 19:34:00 +0300

    keyboardWillShow

--- Interesting numbers/MainViewController.swift
+++ Interesting numbers/MainViewController.swift
@@ -15,6 +15,7 @@ class MainViewController: UIViewController, UITextFieldDelegate {
     @IBOutlet weak var multipleNumbersButton: UIButton!
     @IBOutlet weak var numberTextField: UITextField!
     @IBOutlet weak var displayFactButton: UIButton!
+    @IBOutlet weak var scrollView: UIScrollView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -23,6 +24,10 @@ class MainViewController: UIViewController, UITextFieldDelegate {
         // Додати жест тапу для ховання клавіатури
         let tapGesture = UITapGestureRecognizer(target: self, action: #selector(handleTap))
         view.addGestureRecognizer(tapGesture)
+
+        // Встановіть UIScrollView для відстеження клавіатури
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
     }
     
     func textField(_ textField: UITextField, shouldChangeCharactersIn range: NSRange, replacementString string: String) -> Bool {
@@ -38,6 +43,20 @@ class MainViewController: UIViewController, UITextFieldDelegate {
         // Викликати метод приховання клавіатури для numberTextField
         numberTextField.resignFirstResponder()
     }
-
+    
+    @objc func keyboardWillShow(notification: Notification) {
+        // Піднести UIScrollView вгору при відображенні клавіатури
+        if let keyboardSize = (notification.userInfo?[UIResponder.keyboardFrameEndUserInfoKey] as? NSValue)?.cgRectValue {
+            let contentInsets = UIEdgeInsets(top: 0.0, left: 0.0, bottom: keyboardSize.height, right: 0.0)
+            scrollView.contentInset = contentInsets
+            scrollView.scrollIndicatorInsets = contentInsets
+        }
+    }
+    
+    @objc func keyboardWillHide(notification: Notification) {
+        // Повернути UIScrollView до початкового стану при хованні клавіатури
+        scrollView.contentInset = UIEdgeInsets.zero
+        scrollView.scrollIndicatorInsets = UIEdgeInsets.zero
+    }
 }
 
