cd39ba8a9ce968e45676c400cb4a783093f97996
Stavratiy Andriy <stavraty@gmail.com>
2023-09-11 15:11:18 +0300

    TextViewController

---
+++ Interesting numbers/TextViewController.swift
@@ -0,0 +1,21 @@
+//
+//  TextViewController.swift
+//  Interesting numbers
+//
+//  Created by AS on 11.09.2023.
+//
+
+import UIKit
+
+class TextViewController: UIViewController {
+    
+    
+    @IBOutlet weak var closeButton: UIButton!
+    @IBOutlet weak var numberLabel: UILabel!
+    @IBOutlet weak var factTextView: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
